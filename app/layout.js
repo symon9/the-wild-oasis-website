637b61d3da8d53e509adc86a95e8a0387e674fb0
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-07 19:17:46 +0100

    Add a nested layout

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} relative antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
