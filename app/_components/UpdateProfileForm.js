22eff6238ce7b4105fb9d300b7f26e5824274c32
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-23 21:25:56 +0100

    implement updating a reservation

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { useFormStatus } from "react-dom";
 import { updateGuest } from "../_lib/actions";
+import SubmitButton from "./SubmitButton";
 
 function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
@@ -55,21 +55,9 @@ function UpdateProfileForm({ children, guest }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      disabled={pending}
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-    >
-      {pending ? "Updating" : "Update profile"}
-    </button>
   );
 }
 
