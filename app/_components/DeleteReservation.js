8dadc4f956609d979b142ab832553408ab17cc9c
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-24 00:49:56 +0100

    implement removing reservation immediately on the UI using useOptimistic hook

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -2,15 +2,14 @@
 
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { useTransition } from "react";
-import { deleteReservation } from "../_lib/actions";
 import SpinnerMini from "./SpinnerMini";
 
-function DeleteReservation({ bookingId }) {
+function DeleteReservation({ bookingId, onDelete }) {
   const [isPending, startTransition] = useTransition();
 
   function handleDelete() {
     if (confirm("Are you sure you want to delete this reservation? "))
-      startTransition(() => deleteReservation(bookingId));
+      startTransition(() => onDelete(bookingId));
   }
 
   return (
