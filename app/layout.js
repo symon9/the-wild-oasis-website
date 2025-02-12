88a0e1deb32fbd274e885315f88127a88fbd79db
Simon Emmanuel <120514869+symon9@users.noreply.github.com>
2025-02-12 15:05:49 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,23 +18,18 @@ export const metadata = {
   },
   description:
     "Luxurious cabin hotel, located in the heart of italian Dolomites, surrounded by beautiful mountains and dark forests.",
+viewport: "width=1024, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=1024, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-      </head>
       <body
         className={`${josefin.className} relative antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">
+          <main className="max-w-7xl mx-auto w-full min-w-[1024px]">
             <ReservationProvider>{children}</ReservationProvider>
           </main>
         </div>
